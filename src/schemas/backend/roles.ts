31b0159ed1ba819ad1fa98b80556d1123a17db96
phasenull <55253035+phasenull@users.noreply.github.com>
2025-07-05 18:34:44 +0300

    big time oof

--- src/schemas/backend/roles.ts
+++
@@ -1,6 +0,0 @@
-import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
-
-export const rolesTable = sqliteTable("roles",{
-	id: int().primaryKey({autoIncrement:true}).unique().notNull(),
-	name: text().notNull().unique(),
-})
